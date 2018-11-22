41888b57222884ab948be3770962d2d08ea7fa8c
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-11-22 08:31:17 -0700

    Agregar archivos de proyecto.

---
+++ CambioImagenes/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Diagnostics;
+
+namespace CambioImagenes
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool jugando = true;
+        Pacman pacman;
+        Stopwatch stopwatch = new Stopwatch();
+        TimeSpan tiempoAnterior;
+
+       
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            canvasPrincipal.Focus();
+
+            pacman = new Pacman(spritePacman);
+
+            stopwatch.Start();
+            tiempoAnterior = stopwatch.Elapsed;
+
+
+            ThreadStart threadStart = new ThreadStart(cicloPrincipal);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+
+        }
+
+        public void cicloPrincipal()
+        {
+            while (jugando)
+            {
+                Dispatcher.Invoke(actualizar);
+            }
+        }
+
+        public void actualizar()
+        {
+            TimeSpan tiempoActual = stopwatch.Elapsed;
+            double deltaTime = tiempoActual.TotalSeconds - tiempoAnterior.TotalSeconds;
+
+            pacman.Mover(deltaTime);
+            pacman.Velocidad += 10 * deltaTime;
+
+            tiempoAnterior = tiempoActual;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            jugando = false;
+        }
+
+        private void canvasPrincipal_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                pacman.CambiarDireccion(Pacman.Direccion.Izquierda);
+            }
+            if (e.Key == Key.Right)
+            {
+                pacman.CambiarDireccion(Pacman.Direccion.Derecha);
+            }
+            if (e.Key == Key.Up)
+            {
+                pacman.CambiarDireccion(Pacman.Direccion.Arriba);
+            }
+            if (e.Key == Key.Down)
+            {
+                pacman.CambiarDireccion(Pacman.Direccion.Abajo);
+            }
+        }
+    }
+}
---
+++ CambioImagenes/Pacman.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace CambioImagenes
+{
+    public class Pacman
+    {
+        List<BitmapImage> arriba = new List<BitmapImage>();
+        List<BitmapImage> abajo = new List<BitmapImage>();
+        List<BitmapImage> izquierda = new List<BitmapImage>();
+        List<BitmapImage> derecha = new List<BitmapImage>();
+
+        Image Imagen { get; set; }
+
+        public enum Direccion { Izquierda, Derecha, Arriba, Abajo};
+        Direccion DireccionActual { get; set; }
+
+        double PosicionX { get; set; }
+        double PosicionY { get; set; }
+
+        public double Velocidad { get; set; }
+
+        int spriteActual = 0;
+        double tiempoTranscurridoEnSprite = 0;
+        double tiempoPorSprite = 0.25;
+
+        public Pacman(Image imagen)
+        {
+            Imagen = imagen;
+            arriba.Add(new BitmapImage(new Uri("arriba1.png", UriKind.Relative)));
+            arriba.Add(new BitmapImage(new Uri("arriba2.png", UriKind.Relative)));
+
+            abajo.Add(new BitmapImage(new Uri("abajo1.png", UriKind.Relative)));
+            abajo.Add(new BitmapImage(new Uri("abajo2.png", UriKind.Relative)));
+
+            izquierda.Add(new BitmapImage(new Uri("izquierda1.png", UriKind.Relative)));
+            izquierda.Add(new BitmapImage(new Uri("izquierda2.png", UriKind.Relative)));
+
+            derecha.Add(new BitmapImage(new Uri("derecha1.png", UriKind.Relative)));
+            derecha.Add(new BitmapImage(new Uri("derecha2.png", UriKind.Relative)));
+            Imagen.Source = derecha[0];
+
+            PosicionX = Canvas.GetLeft(imagen);
+            PosicionY = Canvas.GetTop(imagen);
+
+            DireccionActual = Direccion.Derecha;
+
+            Velocidad = 20;
+
+        }
+
+        public void CambiarDireccion(Direccion nuevaDireccion)
+        {
+            DireccionActual = nuevaDireccion;
+            switch (DireccionActual)
+            {
+                case Direccion.Abajo:
+                    Imagen.Source = abajo[0];
+                    break;
+                case Direccion.Arriba:
+                    Imagen.Source = arriba[0];
+                    break;
+                case Direccion.Izquierda:
+                    Imagen.Source = izquierda[0];
+                    break;
+                case Direccion.Derecha:
+                    Imagen.Source = derecha[0];
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Mover(double deltaTime)
+        {
+            tiempoTranscurridoEnSprite += deltaTime;
+            int spriteAnterior = spriteActual;
+            if (tiempoTranscurridoEnSprite >= tiempoPorSprite)
+            {
+                spriteActual++;
+                tiempoTranscurridoEnSprite -= tiempoPorSprite;
+                if (spriteActual > 1)
+                {
+                    spriteActual = 0;
+                }
+            }
+            BitmapImage sprite = null;
+            switch (DireccionActual)
+            {
+                case Direccion.Abajo:
+                    PosicionY += Velocidad * deltaTime;
+                    sprite = abajo[spriteActual];
+                    
+                    break;
+                case Direccion.Arriba:
+                    PosicionY -= Velocidad * deltaTime;
+                    sprite = arriba[spriteActual];
+                    
+                    break;
+                case Direccion.Izquierda:
+                    PosicionX -= Velocidad * deltaTime;
+                    sprite = izquierda[spriteActual];
+                    break;
+                case Direccion.Derecha:
+                    PosicionX += Velocidad * deltaTime;
+                    sprite = derecha[spriteActual];
+                    break;
+                default:
+                    break;
+            }
+            if (spriteAnterior != spriteActual && sprite != null)
+            {
+                Imagen.Source = sprite;
+            }
+            Canvas.SetLeft(Imagen, PosicionX);
+            Canvas.SetTop(Imagen, PosicionY);
+
+
+        }
+    }
+}
